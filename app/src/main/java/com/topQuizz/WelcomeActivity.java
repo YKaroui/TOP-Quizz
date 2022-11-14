05726b6dbe8e685a87ad4a8b5e3c19c401cf7a2e
karoui yassine <karoui.yassine@esprit.tn>
2022-11-14 16:20:30 +0000

    splash screen updated, main layout updated

---
+++ app/src/main/java/com/topQuizz/WelcomeActivity.java
@@ -0,0 +1,30 @@
+package com.topQuizz;
+
+import static java.lang.Thread.sleep;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+
+
+
+    }
+}
